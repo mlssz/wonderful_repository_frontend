69fe7485fee433a2a5b533c0c483100872b19322
lcj <luchenjiemail@gmail.com>
2017-04-24 13:42:43 +0800

    useless

--- src/component/Putaway.js
+++ src/component/Putaway.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class PutAway extends React.Component {
 
@@ -11,6 +13,30 @@ export default class PutAway extends React.Component {
 		return (
 			<div style={{textAlign:'center'}}>
 				<p>请填写物资信息</p>
+				<MuiThemeProvider>
+					<div>
+						<TextField
+					      defaultValue=""
+					      floatingLabelText="物资编号"
+					  	/>
+					  	<TextField
+					      defaultValue=""
+					      floatingLabelText="物资类型"
+					  	/>
+					  	<TextField
+					      defaultValue=""
+					      floatingLabelText="入库时间"
+					  	/>
+					  	<TextField
+					      defaultValue=""
+					      floatingLabelText="估计出库时间"
+					  	/>
+					  	<TextField
+					      defaultValue=""
+					      floatingLabelText="物资描述"
+					  	/>
+				  	</div>
+			    </MuiThemeProvider>
 			</div>
 		)
 	}
