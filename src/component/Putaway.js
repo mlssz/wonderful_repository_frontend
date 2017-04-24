33b1997e328a435aa4a128090c593a9f7d8a643c
lcj <luchenjiemail@gmail.com>
2017-04-24 14:00:25 +0800

    useless

--- src/component/Putaway.js
+++ src/component/Putaway.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class PutAway extends React.Component {
 
 	constructor(props) {
 		super(props);
-
 	}
 
 	render() {
@@ -14,27 +14,41 @@ export default class PutAway extends React.Component {
 			<div style={{textAlign:'center'}}>
 				<p>请填写物资信息</p>
 				<MuiThemeProvider>
-					<div>
+					<div style={{width:"40%",margin:"0 auto"}}>
 						<TextField
 					      defaultValue=""
 					      floatingLabelText="物资编号"
+					      fullWidth={true}
 					  	/>
+					  	<br/>
 					  	<TextField
 					      defaultValue=""
 					      floatingLabelText="物资类型"
+					      fullWidth={true}
 					  	/>
+					  	<br/>
 					  	<TextField
 					      defaultValue=""
 					      floatingLabelText="入库时间"
+					      fullWidth={true}
 					  	/>
+					  	<br/>
 					  	<TextField
 					      defaultValue=""
 					      floatingLabelText="估计出库时间"
+					      fullWidth={true}
 					  	/>
-					  	<TextField
-					      defaultValue=""
-					      floatingLabelText="物资描述"
-					  	/>
+					  	<br/>
+					  	<div style={{textAlign:'start'}}>
+						  	<TextField
+						      defaultValue=""
+						      floatingLabelText="物资描述"
+						      fullWidth={true}
+						      multiLine={true}
+						  	/>
+					  	</div>
+					  	<br/>
+					  	<RaisedButton label="继续"/>
 				  	</div>
 			    </MuiThemeProvider>
 			</div>
