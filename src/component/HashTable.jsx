998292df7a6e670e55274ae3712074b7e95a2262
lcj <luchenjiemail@gmail.com>
2017-04-30 14:43:32 +0800

    add move&in&manage page

--- src/component/HashTable.jsx
+++ src/component/HashTable.jsx
@@ -3,8 +3,17 @@ import {
 	HashRouter,
 	Route
 } from 'react-router-dom'
-import PutAway from './PutAway.jsx'
-import EnsureAction from './EnsureAction.jsx'
+import PutAway from './in/PutAway.jsx'
+import EnsureAction from './in/EnsureAction.jsx'
+import Manage from './in/Manage.jsx'
+
+import Move from './move/Move.jsx'
+import MoveEnsure from './move/EnsureAction.jsx'
+import MoveManage from './move/Manage.jsx'
+
+import Out from './out/out.jsx'
+import OutEnsure from './out/EnsureAction.jsx'
+import OutManage from './out/Manage.jsx'
 
 export default class HashTable extends React.Component {
 	constructor(props) {
@@ -15,9 +24,16 @@ export default class HashTable extends React.Component {
 		return (
 			<HashRouter hashType="noslash">
 				<div>
-					<Route exact path="/" component={PutAway}/>
+					<Route exact path="/" component={Manage}/>
+					<Route path="/putawayManage" component={Manage}/>
 					<Route path="/putaway" component={PutAway}/>
 					<Route path="/putawayEnsure" component={EnsureAction}/>
+					<Route path='/move' component={Move}/>
+					<Route path='/moveEnsure' component={MoveEnsure}/>
+					<Route path='/moveManage' component={MoveManage}/>
+					<Route path='/out' component={Out}/>
+					<Route path='/outEnsure' component={OutEnsure}/>
+					<Route path='/outManage' component={OutManage}/>
 				</div>
 			</HashRouter>
 		)
---
+++ src/component/out/EnsureTable.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import {
+	Table,
+	TableBody,
+	TableRow,
+	TableRowColumn
+} from 'material-ui/Table'
+import Snackbar from 'material-ui/Snackbar';
+import RaisedButton from 'material-ui/RaisedButton'
+
+import {
+	formName,
+	randomNum,
+	makeId,
+	parsePlace,
+	parsetime,
+	changeHash
+} from '../../libs/common.js'
+
+export default class ReactClassName extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			task: {},
+			open: false,
+			mes: '',
+		}
+		this.renderRow = this.renderRow.bind(this);
+		this.closeBar = this.closeBar.bind(this);
+		this.putaway = this.putaway.bind(this);
+	}
+
+	componentWillMount() {
+		let task = JSON.parse(sessionStorage.getItem('outtask'));
+		if (task.from === undefined) {
+			task.from = randomNum(1, 3) + ' 仓 ' + randomNum(1, 10) + ' 架 ' + randomNum(1, 3) + ' 层 ';
+		}
+		task.id = makeId();
+		task.type = task.type.join('-');
+		this.setState({
+			task
+		})
+	}
+
+	renderRow() {
+		let task = this.state.task;
+		let tableBody = [];
+		for (let i in task) {
+			if (!!formName[i]) {
+				let tag = !(i === 'estimated_export_time');
+				let row =
+					<TableRow key={i}>
+				        <TableRowColumn style={{fontSize:16}}>{formName[i]}</TableRowColumn>
+				        <TableRowColumn style={{fontSize:16}}>{tag?task[i]:parsetime(task[i],0)}</TableRowColumn>
+			    	</TableRow>
+				tableBody.push(row);
+			}
+		}
+		return tableBody;
+	}
+
+	cancel() {
+		changeHash('out');
+	}
+
+	putaway() {
+		sessionStorage.removeItem('task');
+		this.setState({
+			mes: '移动成功，3秒后返回移动页面',
+			open: true,
+		})
+		setTimeout(() => changeHash('out'), 3000);
+	}
+
+	closeBar(tab = 0) {
+		this.setState({
+			open: false,
+		})
+		if (tab === 1)
+			changeHash('out');
+	}
+
+	render() {
+		return (
+			<div>
+				<Table
+					selectable={false}
+					style={{width:"80%",margin:"0 auto"}}>
+				    <TableBody
+				    	displayRowCheckbox={false}
+				    	deselectOnClickaway={false}>
+				      {this.renderRow()}
+				    </TableBody>
+				</Table>
+				<div style={{display:'flex',flexDirection:'row',justifyContent:'space-around',width:'80%',margin:'20 auto'}}>
+					<RaisedButton label="重新填写" primary={true} onTouchTap={this.cancel}/>
+					<RaisedButton label="确认" primary={true} onTouchTap={this.putaway}/>
+				</div>
+				<Snackbar
+					open={this.state.open}
+					message={this.state.mes}
+					autoHideDuration={3000}
+					onRequestClose={this.closeBar}
+					action="立即跳转"
+					onActionTouchTap={()=>this.closeBar(1)}
+		        />
+			</div>
+		)
+	}
+}
