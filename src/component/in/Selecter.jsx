5baed126f84d0e734e3bad983464d5bfd60d6165
lcj <luchenjiemail@gmail.com>
2017-05-07 20:22:08 +0800

    alpha

--- src/component/in/Selecter.jsx
+++ src/component/in/Selecter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Searcher from "../Searcher.jsx"
+import Paper from 'material-ui/Paper'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class ReactClassName extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			paperDisplay: false
+		}
 	}
 
 	onSearchTouchTap(v) {
@@ -30,10 +35,24 @@ export default class ReactClassName extends React.Component {
 		}];
 		return (
 			<div>
-				<Searcher
-					searchKeys={searchKeys}
-					onSearchTouchTap={this.onSearchTouchTap.bind(this)}
-					onShowAllTouchTap={this.onShowAllTouchTap.bind(this)}/>
+				<div style={{height:56}}>
+					<span style={{fontSize:20,lineHeight:"56px",marginLeft:20}}>按照条件筛选</span>
+					<RaisedButton
+						label={this.state.paperDisplay?"隐藏":"显示"}
+						primary={true}
+						onTouchTap={()=>{
+							let paperDisplay =!this.state.paperDisplay;
+							this.setState({paperDisplay})
+						}}
+						style={{float:"right",margin:10}}/>
+					<div style={{clear:'both'}}></div>
+				</div>
+				<Paper style={{display:this.state.paperDisplay?'block':'none'}}>
+					<Searcher
+						searchKeys={searchKeys}
+						onSearchTouchTap={this.onSearchTouchTap.bind(this)}
+						onShowAllTouchTap={this.onShowAllTouchTap.bind(this)}/>
+				</Paper>
 			</div>
 		)
 	}
